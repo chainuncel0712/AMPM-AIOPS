8308cdbf281636383676e16ce5c3a33e95bc39f8
pop5057273712_gmail_com <pop5057273712@gmail.com>
2026-05-20 10:50:01 +0000

    auto-sync: 2026-05-20T10:50:01+00:00

--- outputs/同步黑曜作品.bat
+++ outputs/同步黑曜作品.bat
@@ -10,7 +10,7 @@ set DESKTOP=%USERPROFILE%\Desktop\黑曜作品集
 if not exist "%DESKTOP%" mkdir "%DESKTOP%"
 
 echo 📥 正在從 VPS 下載黑曜的作品...
-scp -i %USERPROFILE%\.ssh\id_ed25519 -r pop5057273712_gmail_com@35.187.206.192:outputs/* "%DESKTOP%\"
+scp -i %USERPROFILE%\.ssh\id_ed25519 -r pop5057273712_gmail_com@35.187.206.192:AMPM-AIOPS/outputs/* "%DESKTOP%\"
 
 if %errorlevel% equ 0 (
     echo.
